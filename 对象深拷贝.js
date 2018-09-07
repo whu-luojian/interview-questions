069033f451ca9df04606ccfcb289d52630c6cc49
luojian3 <luojian3@jd.com>
2018-09-07 18:12:49 +0800

    递归深拷贝 + 宽度优先深拷贝

---
+++ 对象深拷贝.js
@@ -0,0 +1,91 @@
+/**
+ * 利用递归进行对象深拷贝
+ * @param {*} obj 
+ */
+function deepColne(obj) {
+  let newObj
+  if(!(obj instanceof Object)) {
+    return obj
+  } 
+
+  if (obj instanceof Date) {
+    return new Date(obj)
+  } 
+
+  if (obj instanceof RegExp) {
+    return new RegExp(obj)
+  } 
+
+  if (obj instanceof Array) {
+    newObj = []
+    obj.forEach(item => {
+      newObj.push(deepColne(item))
+    })
+  } else {
+    newObj = {}
+    for(let key of Object.keys(obj)) {
+      newObj[key] = deepColne(obj[key])
+    }
+  }
+  return newObj
+}
+
+let a = {
+  a: 2,
+  c: {
+    b: 2,
+    e: [1, 1, 1]
+  }
+}
+console.log(deepColne2(a))
+
+/**
+ * 利用宽度优先和队列实现深拷贝，解决出现环的问题
+ * @param {*} obj 
+ */
+function deepColne2(obj) {
+  if(!(obj instanceof Object)) {
+    return obj
+  }
+
+  if(obj instanceof Date) {
+    return new Date(obj)
+  }
+
+  if(obj instanceof RegExp) {
+    return new RegExp(obj)
+  }
+
+  let newObj = obj instanceof Array ? [] : {}
+  let srcQue = [obj]
+  let srcVisitedQue = []
+  let copyQue = [newObj]
+  let copyVisitedQue = []
+  while(srcQue.length > 0) {
+    let curSrcElement = srcQue.shift()
+    let curCopyElement = copyQue.shift()
+    srcVisitedQue.push(curSrcElement)
+    copyVisitedQue.push(curCopyElement)
+
+    for(let key in curSrcElement) {
+      if(!(curSrcElement[key] instanceof Object)) {
+        curCopyElement[key] = curSrcElement[key]
+      } else if(curSrcElement[key] instanceof Date) {
+        curCopyElement[key] = new Date(curSrcElement[key])
+      } else if(curSrcElement[key] instanceof RegExp) {
+        curCopyElement[key] = new RegExp(curSrcElement[key])
+      } else {
+        //判断是否出现环，即是否被访问
+        let index = srcVisitedQue.indexOf(curSrcElement[key])
+        if(index !== -1) {
+          curCopyElement[key] = srcVisitedQue[index]
+        } else {
+          curCopyElement[key] = curSrcElement[key] instanceof Array ? [] : {}
+          srcQue.push(curSrcElement[key])
+          copyQue.push(curCopyElement[key])
+        }
+      }
+    }
+  }
+  return newObj
+}
