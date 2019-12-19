b6622ab4da21b61fad8c9d891f04ec2c87f3aaa2
luojian <1499596970@qq.com>
2019-12-19 11:11:55 +0800

    feat(): add new

--- 手写代码/new.js
+++ 手写代码/new.js
@@ -1,15 +1,15 @@
 /**
- * 1. 创建一个新对策
+ * 1. 创建一个新对象
  * 2. 新对象的原型指向构造函数的原型
  * 3. 绑定this
- * 4. 返回新对象
+ * 4. 确定返回值
  */
 // create函数接收的第一个参数为构造函数，其余为构造函数的参数
  function create() {
    // 创建一个空对象
    let obj = {}
    // 获取构造函数
-   let Con = [].shift.call(arguments)
+   let Con = Array.prototype.shift.call(arguments)
    // 链接到原型
    Object.setPrototypeOf(obj, Con.prototype)
    // 绑定this，执行构造函数
